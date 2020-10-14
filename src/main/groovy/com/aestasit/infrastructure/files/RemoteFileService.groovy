823a5b4d64cb0e0c2ab26624fa87af40e2f645c1
Andres Almiray <aalmiray@gmail.com>
2020-10-15 00:03:44 +0200

    Update build

--- src/main/groovy/com/aestasit/infrastructure/files/RemoteFileService.groovy
+++ src/main/groovy/com/aestasit/infrastructure/files/RemoteFileService.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011-2020 Aestas/IT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.aestasit.infrastructure.files
 
 import com.aestasit.infrastructure.exceptions.ConfigurationException
@@ -59,7 +74,7 @@ class RemoteFileService {
 
   boolean mkdir(String destination) {
     // TODO: improve error handling
-    exec "mkdir -p ${destination}"
+    executionContext.execute "mkdir -p ${destination}"
   }
 
   boolean remkdir(String destination) {
@@ -78,12 +93,12 @@ class RemoteFileService {
 
   boolean delete(String destination) {
     // TODO: improve error handling
-    exec "rm -rf ${destination}"
+    executionContext.execute "rm -rf ${destination}"
   }
 
   boolean touch(String destination) {
     // TODO: improve error handling
-    exec "touch ${destination}"
+    executionContext.execute "touch ${destination}"
   }
 
   // TODO: Allow remote file manipulation through closure.
--- src/test/groovy/com/aestasit/infrastructure/files/RemoteURLTest.groovy
+++ src/test/groovy/com/aestasit/infrastructure/files/RemoteURLTest.groovy
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011-2020 Aestas/IT
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.aestasit.infrastructure.files
 
 import com.aestasit.infrastructure.RemoteURL
@@ -6,7 +21,6 @@ import groovy.transform.TypeChecked
 import org.junit.Test
 
 @CompileStatic
-@TypeChecked
 class RemoteURLTest extends GroovyTestCase {
 
   @Test
